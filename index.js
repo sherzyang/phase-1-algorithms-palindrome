ea89e643d523c4b57c53fdee4936890cae678fe9
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-03-08 22:33:59 -0800

    Update response.

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+  // save lowercase of word to variable originalWord
+  let originalWord = word.toLowerCase();
+  
+  // declare a variable for letter 
+  let letter;
+
+  // declare a variable reverseArray and assign to empty array 
+  let reverseArray = [];
+
+  // iterate through each letter of originalWord from the last letter to the first 
+  // take the last letter and add it to reverseArray
+  for (i = originalWord.length -1; i>=0; i--){
+    letter = originalWord[i];
+    reverseArray.push(letter);
+  }
+
+  // join reverseArray as one string to variable secondWord
+  let secondWord = reverseArray.join('')
+
+  // Use if statement to compare if originalWord is equal to secondWord
+  if (originalWord == secondWord){
+    return true 
+  }
+  else {
+    return false
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  1. Save lowercase of word to variable originalWord
+  2. Declare a variable for letter 
+  3. Declare a variable reverseArray and assign to empty array 
+  4. Iterate through each letter of originalWord from the last letter to the first 
+      take the last letter and add it to reverseArray
+  5. Join reverseArray as one string to variable secondWord 
+  6. Use if statement to compare if originalWord is equal to secondWord
 */
 
 /*
   Add written explanation of your solution here
+  I need a function that will give me a word with its letters in reverse 
+  and compare that word in reverse to the original word. 
 */
 
 // You can run `node index.js` to view these console logs
